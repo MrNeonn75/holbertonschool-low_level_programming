c696bea7fb072a6e4ffee5cf52c1e148d4046b72
kali <kali@kali.kali>
2023-10-19 09:53:30 +0400

    Git upload script

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,6 +8,31 @@
 #include <time.h>
 #include <string.h>
 
+/* Fuctions */
+int get_last_digit (int n)
+{
+/* Variables */
+char random_string_number[100];
+char last_number;
+int number_size;
+int last_digit;
+
+/* Convert int to string */
+sprintf(random_string_number, "%d", n);
+
+/* Get last digit */
+sprintf(random_string_number, "%d", n);
+number_size = strlen(random_string_number);
+last_number = random_string_number[number_size];
+
+/* Convert char to int */
+last_digit = last_number - '0';
+
+/* Return the number */
+return last_digit;
+
+}
+
 /**
  * main - Prints last digit of random number
  *
@@ -17,21 +42,16 @@ int main(void)
 {
 int n;
 char random_string_number[100];
-int number_size;
-char last_number;
-int last_digit;
 char last_string[50];
 char text[] = "Last digit of";
+int last_digit = get_last_digit(n);
+last_digit = get_last_digit(n);
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
+/* Convert int to string */
 sprintf(random_string_number, "%d", n);
-number_size = strlen(random_string_number);
-last_number = random_string_number[number_size - 1];
-
-/* Convert char to int */
-last_digit = last_number - '0';
 
 if (n >= 0)
 {
@@ -55,18 +75,18 @@ strcpy(last_string, "less than 6 and not 0\n");
 
 if (n < 0)
 {
-printf("%s %s is -%c and is %s", 
+printf("%s %s is -%d and is %s", 
 text, 
 random_string_number, 
-last_number, 
+last_digit, 
 last_string);
 }
 else
 {
-printf("%s %s is %c and is %s", 
+printf("%s %s is %d and is %s", 
 text, 
 random_string_number, 
-last_number, 
+last_digit, 
 last_string);
 }
 
