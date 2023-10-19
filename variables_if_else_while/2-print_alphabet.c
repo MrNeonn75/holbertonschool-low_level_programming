57d1be97d763cbe2179a870c629b3a713d2e8bfa
kali <kali@kali.kali>
2023-10-19 10:16:25 +0400

    Git upload script

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: 7727@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet in lowecase, followed by new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char letter;
+
+for(letter = 'a'; letter < 'z'; letter++)
+{
+putchar(letter);
+}
+
+return (0);
+
+}
