5363c7ef72a4da83115ad288241d3de5ce53e94a
kali <kali@kali.kali>
2023-10-19 12:08:07 +0400

    Git upload script

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-putchar.c
+ * Author: 7727@holbertonschool.com
+ */
+
+
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+puts("_putchar");
+
+return (0);
+
+}
