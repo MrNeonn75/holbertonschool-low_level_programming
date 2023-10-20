eacf7d8d1e0e4aa24fd0d6179c93615d99bffce0
kali <kali@kali.kali>
2023-10-20 23:01:39 +0400

    Git upload script

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+/**
+ * main - Prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+
+return (0);
+
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - Prints 10 tmies alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+int len = strlen(alphabet);
+int i;
+int time;
+
+for (time = 0; time <10; time++)
+{
+
+for (i = 0; i < len; i++)
+{
+_putchar(alphabet[i]);
+}
+_putchar(10);
+
+}
+
+}
+
