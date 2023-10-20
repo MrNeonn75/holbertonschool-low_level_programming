716d4a864c664846f3e8ff32fb34dfca5237b61a
kali <kali@kali.kali>
2023-10-20 10:12:19 +0400

    Git upload script

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -18,6 +18,14 @@ putchar(10);
 void print_alphabet()
 {
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+int len = strlen(alphabet);
+int i;
 
-__putchar(alphabet);
+for(i = 0; i < len; i++)
+{
+putchar(alphabet[i]);
+}
+
+putchar(10);
+
 }
