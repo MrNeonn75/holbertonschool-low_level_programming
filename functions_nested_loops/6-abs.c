3046bf978a4e5ccd605caee13f4f58ed2c205ce3
kali <kali@kali.kali>
2023-10-22 00:07:06 +0400

    Git upload script

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,16 +1,16 @@
 /*
- * File: 5-sign.c
+ * File: 6-abs.c
  * Author: 7727@holbertonschool.com
  */
 
 #include "main.h"
 
 /**
- * print_sign - A function that prints the sign of a number.
- * @c: The character to be checked.
+ * _abs - A function that computes the absolute value of an integer.
+ * 
+ * @x: The character to be checked.
  *
- * Return: 1 if the number is greater than 0,
- *  0 - if number equal to 0, -1 otherwise.
+ * Return: Absolute value of an integer.
  */
 
 int _abs(int x)
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+/*
+ * File: 7-print_last_digit.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - A function that computes the absolute value of an integer.
+ * 
+ * @x: The character to be checked.
+ *
+ * Return: Absolute value of an integer.
+ */
+
+int print_last_digit(int x)
+{
+int last_digit = x % 10;
+
+if (last_digit < 0)
+{
+last_digit = last_digit * -1;
+}
+
+printf("%d", last_digit);
+return (last_digit);
+
+}
