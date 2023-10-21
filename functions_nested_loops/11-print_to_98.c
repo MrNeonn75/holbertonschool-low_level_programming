356e22f951fb8778a46c46e10dae70969a35fc32
kali <kali@kali.kali>
2023-10-22 02:23:26 +0400

    Git upload script

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 {
 if (n > 98)
 {
-for (;n >= 98; n--)
+for (; n >= 98; n--)
 {
 if (n != 98)
 {
@@ -32,7 +32,7 @@ printf("%d\n", n);
 }
 else
 {
-for(;n <= 98; n++)
+for (; n <= 98; n++)
 {
 if (n != 98)
 {
