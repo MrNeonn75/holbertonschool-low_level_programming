a9144c2d66758a4f4536c3048deaf91a8a548bda
kali <kali@kali.kali>
2023-10-22 00:23:06 +0400

    Git upload script

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -18,16 +18,10 @@ int print_last_digit(int x)
 int last_digit = x % 10;
 
 if (last_digit < 0)
-{
-last_digit = last_digit * -1;
+    last_digit *= -1;
 
-_putchar((char)last_digit);
+_putchar(last_digit + '0');
+
 return (last_digit);
-}
-else
-{
-_putchar((char)last_digit);
-return (last_digit);
-}
 
 }
