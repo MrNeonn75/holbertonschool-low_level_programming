cdae9323010dcb03d9c425800f7eabdd4fc979e6
kali <kali@kali.kali>
2023-10-22 02:05:19 +0400

    Git upload script

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * File: 10-add.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+ * times_table - A function that adds two integers and returns the result.
+ *
+ * Return: Nothing.
+ */
+
+int add(int a, int b)
+{
+return a + b;
+}
