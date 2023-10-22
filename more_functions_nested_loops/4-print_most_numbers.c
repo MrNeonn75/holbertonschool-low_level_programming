12080bb51b1312689cb197f6c1eb060b26d9bc0c
kali <kali@kali.kali>
2023-10-22 17:05:25 +0400

    Git upload script

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,19 +6,22 @@
 #include "main.h"
 
 /**
- * print_numbers - A function that prints the numbers, from 0 to 9,
- *  followed by a new line.
+ * print_most_numbers - A function that prints the numbers, from 0 to 9,
+ *  followed by a new line, without 2 and 4.
  * 
  *
  * Return: 1 if c is uppercase, 0 otherwise.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 for (i = 48; i < 58; i++)
 {
+if (i != 50 && i !=52)
+{
 _putchar(i);
+}
 }
 _putchar(10);
 
