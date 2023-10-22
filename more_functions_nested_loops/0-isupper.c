7bc6ac7ca4c58b8ba0cc338d5f2b9d90b41e29e2
kali <kali@kali.kali>
2023-10-22 16:38:08 +0400

    Git upload script

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-isupper.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: Choosen character (ASCII)
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
