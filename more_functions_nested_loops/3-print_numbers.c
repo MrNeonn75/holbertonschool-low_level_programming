b49ee806f0a4c6941fc60881ca59565bd6d20110
kali <kali@kali.kali>
2023-10-22 17:00:19 +0400

    Git upload script

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-mul.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+ * mul - A function that prints the numbers, from 0 to 9,
+ *  followed by a new line.
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+
+}
