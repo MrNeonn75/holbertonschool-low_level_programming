50f6ce71c46f0824e5f61d249bbe52f6a38df557
kali <kali@kali.kali>
2023-10-23 10:46:04 +0400

    Git upload script

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+/*
+ * File: 6-abs.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that updates the value it points to to 98.
+ * @*n: A pointer.
+ *
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+
+}
