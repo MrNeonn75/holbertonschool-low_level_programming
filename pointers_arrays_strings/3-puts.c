cc78b792329fea8beb989407b17026279d46e048
kali <kali@kali.kali>
2023-10-24 10:52:43 +0400

    Git upload script

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-puts.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - A function that prints the string, followed by new line.
+ * @str: String.
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+puts(str);
+
+}
+
