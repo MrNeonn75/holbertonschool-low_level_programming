ff573d6f220ed402291bd0bbf123db2f6ee03f1d
kali <kali@kali.kali>
2023-10-24 11:01:11 +0400

    Git upload script

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-puts.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - A function that prints the string, followed by new line.
+ * @str: String.
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+int len = (int)strlen(s);
+int i;
+
+for (i = len; i >= 0; i--)
+{
+    _putchar(s[i]);
+}
+_putchar(10);
+
+}
+
