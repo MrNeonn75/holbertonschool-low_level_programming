29f5d817c7869a004aebbac03941b5129712c3f1
kali <kali@kali.kali>
2023-10-27 10:53:32 +0400

    Git upload script

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -18,20 +18,25 @@
 
 void print_array(int *a, int n)
 {
-    int i;
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+if (i != n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d\n", a[i]);
+}   
 
-    for (i = 0; i < n; i++)
-    {
-        if (i != n - 1)
-        {
-            printf("%d, ", a[i]);
-        }
-        else
-        {
-            printf("%d\n", a[i]);
-        }
-        
-        
-    }
+}
+}
+else
+{
+printf("\n");
+}
 
 }
