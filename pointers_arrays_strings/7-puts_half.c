84cdbecccfdc01a08847b252ff657b6a2a9f2d6a
kali <kali@kali.kali>
2023-10-27 10:33:38 +0400

    Git upload script

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+/*
+ * File: 6-puts2.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - A function that prints half of a string, followed by a new line.
+ *  starting with the first character,
+ *  followed by a new line.
+ * @str: String.
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+int index = 0, len = 0, n;
+
+while (str[index++])
+len++;
+
+if ((len % 2) == 0)
+n = len / 2;
+
+else
+n = (len + 1) / 2;
+
+for (index = n; index < len; index++)
+_putchar(str[index]);
+
+_putchar('\n');
+}
