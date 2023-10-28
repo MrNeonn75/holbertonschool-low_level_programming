623e3947a00cb48238d01302445669ca5a2edc70
kali <kali@kali.kali>
2023-10-28 20:10:31 +0400

    Git upload script

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,6 +3,7 @@
  * Author: 7727@holbertonschool.com
  */
 
+#include <string.h>
 #include "main.h"
 
 /**
@@ -15,15 +16,7 @@
  * Return: The value of pointer dest
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (dest);
+	strcpy(dest, src);
 }
