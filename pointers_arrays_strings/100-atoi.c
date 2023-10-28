3788158cafde747fbd8ec7ec137e40736c7e4cc5
kali <kali@kali.kali>
2023-10-28 20:00:21 +0400

    Git upload script

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+/*
+ * File: 100-atoi.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _atoi - A function that convert a string to an integer.
+ * @s: Variable
+ *
+ * Return: Nothing
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+/*
+ * File: 9-strcpy.c
+ * Author: 7727@holbertonschool.com
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - A function that copies the string pointed to by src,
+ *  including the terminating null byte (\0),
+ *  to the buffer pointed to by dest.
+ * @dest: Variable
+ * @str: String
+ *
+ * Return: The value of pointer dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
