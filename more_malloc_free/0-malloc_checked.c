6dc7d2d16e4699dde17bbd14afdfc0acaf98b143
kali <kali@kali.kali>
2023-11-19 15:13:19 +0400

    Git upload script

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked -  allocates memory using malloc.
- * @b: size
- *
- * Return: Pointer to allocated memory or normal process termination
- * with a status value of 98
- */
+* malloc_checked -  allocates memory using malloc.
+* @b: size
+*
+* Return: Pointer to allocated memory or normal process termination
+* with a status value of 98
+*/
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	void *variable;
 
-	if (mem == NULL)
+	variable = malloc(b);
+
+	if (variable == NULL)
 		exit(98);
 
-	return (mem);
+	return (variable);
 }
