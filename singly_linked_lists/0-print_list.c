f66d34f8edd6df4393d232945c89553886e04bad
kali <kali@kali.kali>
2023-12-10 14:47:19 +0400

    Project C - Signly linked lists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-#include <stddef.h>
 #include "lists.h"
+#include <stddef.h>
 
 /**
- * print_list - The function that prints all the elements of a list_t list.
- * @h - Head.
- * 
- * Return: The number of nodes.
-*/
-size_t print_list(const list_t *h){
-    int count = 0;
+ * print_list - prints a singly linked list
+ * @h: pointer to head of singly linked list
+ *
+ * Return: Number of elements in list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int size = 0;
 
-    while (h->next != NULL)
-    {
-        count++;
-
-        if (h->str == NULL)
-        {
-            printf("(null)");
-        }
-        
-
-        h = h->next;
-    }
-
-    count++;
-
-    return count;
+	while (h != NULL)
+	{
+		if (h->str != 0)
+			printf("[%i] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		size++;
+	}
+	return (size);
 }
