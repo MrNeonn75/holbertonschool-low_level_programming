8436c813cf0f3fd62e2c7978706d2d838dbb5db3
kali <kali@kali.kali>
2023-12-16 17:43:33 +0400

    Git upload script

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <fcntl.h>
 
 /**
  * sum_them_all -  sums all arguments variadic
@@ -15,8 +15,9 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     char *str;
-    FILE *ptr =fopen(filename, "r");
-    char *return_variable;
+    int ptr, size;
+
+    ptr = open(filename, O_RDONLY);
 
     str = malloc(letters * sizeof(char));
 
@@ -25,19 +26,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
         return (0);
     }
     
-    if (ptr == NULL)
+    if (ptr < 0)
     {
         return (0);
     }
 
-    fread(&str, sizeof(char), letters, ptr);
+    size = read(ptr, str, letters);
 
-    sprintf(return_variable, "%s\n(printed chars: %ld)", str, letters);
+    write(1, &str, letters);
 
-    write(1, &return_variable, strlen(return_variable));
+    close(ptr);
 
-    fclose(ptr);
-
-    return (strlen(return_variable));
+    return (size);
 
 }
